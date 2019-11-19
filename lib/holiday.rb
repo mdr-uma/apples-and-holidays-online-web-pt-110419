a961dc5a1d78797f49a1dd0d08c9d68484978c6e
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-19 23:03:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,5 +76,11 @@ def all_supplies_in_holidays(holiday_hash)
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.map do |season, holidays
+  holiday_hash.map do |season, holidays|
+    holidays.map do |event, supplies|
+      if supplies.include? ("BBQ")
+        event
+      end
+    end
+  end.flatten.compact
 end
