e16a08a73696cca577bfb6ee632b3e002d064e21
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-20 00:18:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
       # binding.pry
       puts "#{season.to_s.capitalize}:" holiday.each do |event, supplies|
-        puts "#{event.to_s.split(",").map {|x|x.capitalize}.join(",")}:
+        puts "#{event.to_s.split("_").map {|x|x.capitalize}.join(",")}:
         #{supplies.join(",")}"
       end
     end
